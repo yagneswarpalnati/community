c088192d508b9d55d17d764b9d692d33d1d2ed07
yagneswarpalnati <palnatiyagneswar25@gmail.com>
2025-12-23 10:42:12 +0530

    shared files

--- shared/src/lib/shared.tsx
+++ shared/src/lib/shared.tsx
@@ -1,6 +1,6 @@
 import styles from './shared.module.css';
 
-export function Shared({title}) {
+export function Shared({title}:{title:string}) {
   return (
     <div className={styles['container']}>
       <h1>Welcome to {title}</h1>
